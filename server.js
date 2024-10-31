cc35077a318b16bd1ce4fdbe08ffcba0e2edc07e
djubx <dheeraj.jha.ub@gmail.com>
2024-10-31 11:28:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
